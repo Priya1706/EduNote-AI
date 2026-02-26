9e7e14a8d3f739cf2d5b394d597d1098f313a03f
Priya1706 <74455327+Priya1706@users.noreply.github.com>
2026-02-27 00:19:39 +0530

    WIFI transfer added
    The hardware is now powered by a power bank and audio is streamed to laptop using WIFI

--- main_program/audio_rec.py
+++ main_program/audio_rec.py
@@ -1,37 +1,46 @@
-import serial
+import socket
 import wave
 import time
 import os
 
-PORT = "COM10"
-BAUD_RATE = 921600
+ESP_IP = "192.168.137.151"
+PORT = 5000
 SAMPLE_RATE = 16000
 OUTPUT_FILE = "main_program/live_recording.wav"
 
 def record_audio(duration):
-    print("🎙 Opening serial port...")
-    ser = serial.Serial(PORT, BAUD_RATE, timeout=0.1)
-    time.sleep(2)
 
-    print("▶ Sending start signal to ESP...")
-    ser.write(b'1')
+    print("Connecting to ESP...")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ESP_IP, PORT))
 
-    print(f"🎙 Recording for {duration} seconds...")
+    # VERY IMPORTANT
+    sock.settimeout(0.2)
 
+    print("Sending START")
+    sock.sendall(b"START\n")
+
+    print(f"Recording for {duration} seconds...")
     start_time = time.time()
     audio_data = bytearray()
 
-    while time.time() - start_time < duration:
-        chunk = ser.read(4096)
-        if chunk:
-            audio_data.extend(chunk)
+    while True:
+        if time.time() - start_time >= duration:
+            break
 
-    ser.close()
+        try:
+            data = sock.recv(4096)
+            if data:
+                audio_data.extend(data)
+        except socket.timeout:
+            pass
 
-    print("Recording finished")
+    print("Sending STOP")
+    sock.sendall(b"STOP\n")
+    sock.close()
+
     print("Bytes captured:", len(audio_data))
-
-    return bytes(audio_data)
+    return audio_data
 
 
 def save_wav(audio_data):
@@ -43,7 +52,7 @@ def save_wav(audio_data):
         wf.setframerate(SAMPLE_RATE)
         wf.writeframes(audio_data)
 
-    print("✅ Audio saved as:", OUTPUT_FILE)
+    print("Saved:", OUTPUT_FILE)
 
 
 if __name__ == "__main__":
